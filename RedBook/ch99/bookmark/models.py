9fc0087ec1eb3df495160f6b03413d26d1f42534
EunjaeJang <ejjang2030@gmail.com>
2021-03-25 15:26:21 +0900

    2021.03.25-15:20 : 테이블 생성 1. bookmark추가 2. bookmark/models.py에 테이블 column추가 3. bookmark/admin.py에 테이블 이름 추가

--- RedBook/ch99/bookmark/models.py
+++ RedBook/ch99/bookmark/models.py
@@ -1,3 +1,15 @@
-from django.db import models
-
 # Create your models here.
+
+from django.db import models
+import datetime
+
+
+class Bookmark(models.Model):
+    title = models.CharField('TITLE', max_length=100, blank=True)
+    url = models.URLField('URL', unique=True)
+    memo = models.CharField('MEMO', max_length=1024, blank=True)
+    # registered_time = models.DateTimeField('REGISTERED_TIME', null=True, blank=True)
+    registered_time = models.DateTimeField('REGISTERED_TIME', default=datetime.datetime.now, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
